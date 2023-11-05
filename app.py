1086f3abf910dc2255d17d0ea226cfb2219dc587
Shreyash <shreyashthengne2003@gmail.com>
2023-11-06 00:55:23 +0530

    Updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import mysql.connector
 import pandas as pd
 
@@ -6,21 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def launch():
-    mydb = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='Pavan@123'
-    )
-
-    cursor = mydb.cursor()
-    cursor.execute("use music_db1")
-
     cursor.execute("select name from song_list")
     name = []
     for i in cursor:
         name.append(i[0])
     
-    cursor.execute("select duration_ms from song_list")
+    cursor.execute("select duration from song_list")
     duration = []
     for i in cursor:
         duration_ms = i[0]
@@ -38,29 +29,78 @@ def launch():
         cursor.execute(f"select artists from artist_mapped_id where id = '{i[0]}'")
         artist_one = []
         for j in cursor:
-            artist_one.append(j)
+            artist_one.append(j[0])
         artists.append(artist_one)
+
+    length = range(len(name))
+
+    return render_template('index.html', zipped = zip(name, artists, duration, length))
+
+
+
+@app.route("/history", methods = ['POST'])
+def update_history():
+    data = request.json
+    print(data)
+    # print(type(data))
+    # print(data['data'])
+
+    cursor.execute(f"Select id from song_list where name = '{data['data']}'")
+    for i in cursor:
+        id = i[0]
     
-    # for i in cursor:
-    #     artist.append(i)
+    try:
+        cursor.execute(f"insert into history(id) values ('{id}')")
+    except:
+        cursor.execute(f"delete from history where id = '{id}'")
+        print('deleted')
+        cursor.execute(f"insert into history(id) values ('{id}')")
+    mydb.commit()
+    return jsonify(success=True)
+
+@app.route("/show_history")
+def show_hist():
+    id = []
+    name = []
+    duration = []
+    artists=[]
+    cursor.execute("select id from history order by no desc")
+    for i in cursor:
+        id.append(i[0])
+        
+    for i in id:
+        cursor.execute(f"select name from song_list where id = '{i}'")
+        for j in cursor:
+            name.append(j[0])
+        
+        cursor.execute(f"select duration from song_list where id = '{i}'")
+        for j in cursor:
+            duration_ms = j[0]
+            duration_min = duration_ms // 60000
+            duration_sec = (duration_ms % 60000) // 1000
+            duration.append(f"{duration_min}:{duration_sec:02}")
+
+        cursor.execute(f"select artists from artist_mapped_id where id = '{i}'")
+        artist_one = []
+        for j in cursor:
+            artist_one.append(j[0])
+        artists.append(artist_one)
+
+
+    length = range(len(name))
     
-    # df = pd.DataFrame()
-    # df_list = []
-    # for i in cursor:
-    #     df_list.append(list(i))
-    # df = pd.DataFrame(df_list)
+    return render_template('history.html', zipped = zip(name, zip(artists), duration, length))
 
-    
-    return render_template('index.html', zipped = zip(name, artists, duration))
-
-
-
-
-
-
-
-
+        
 if __name__ == "__main__":
+    mydb = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='Shreyash2003'
+    )
+    cursor = mydb.cursor()
+    cursor.execute("use music_db")
+
     app.run(debug = True)
     
     
